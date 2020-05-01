06ac68c029f10cc9bd7b50fc17a89586e09d7900
user <user@192.168.1.11>
2020-05-01 10:47:21 +0800

    no msg

---
+++ src/main/java/com/zpc/springcloud/eureka/demo/App.java
@@ -0,0 +1,13 @@
+package com.zpc.springcloud.eureka.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
